67919735a0d67139b958c15e31b970ac43eac3a0
gowens <gowens@cmu.edu>
2012-11-24 01:33:15 -0500

    Potentially capable of reading tag.

---
+++ code/src/com/example/ap/verifier/FormatConverter.java
@@ -0,0 +1,69 @@
+package com.example.ap.verifier;
+
+import com.google.common.base.CharMatcher;
+
+public class FormatConverter {
+
+	public static String hexToString(String hex)
+	{	
+		int decimal = 0;
+		StringBuilder buffer = new StringBuilder();
+		 
+		for( int i=0; i<hex.length()-1; i+=2 )
+		{
+			//grab the hex in pairs
+			String output = hex.substring(i, (i + 2));
+			try{
+				//convert hex to decimal
+				decimal = Integer.parseInt(output, 16);
+				//convert the decimal to character
+				buffer.append((char)decimal);				
+			} catch(Exception nfe) { }
+		}
+		
+		//remove Non-ASCII characters
+		String data = CharMatcher.ASCII.retainFrom(buffer.toString());
+		return data.toUpperCase();
+	}
+	
+	public static String hexToDecimal(String hex)
+	{	  
+		StringBuilder buffer = new StringBuilder();
+		int decimal = 0;
+		
+		for( int i=0; i<hex.length()-1; i+=2 )
+		{
+			//grab the hex in pairs
+			String output = hex.substring(i, (i + 2));
+			
+			try{
+				//convert hex to decimal
+				decimal = Integer.parseInt(output, 16);
+				//convert the decimal to character
+				buffer.append(decimal);				
+			} catch(Exception nfe) { }
+		}
+		 
+		return buffer.toString();
+	}	
+	
+	public static String byteArrayToHexString(byte[] byteArray) 
+	{
+		StringBuffer buffer = new StringBuffer(byteArray.length * 2);
+		
+		for (int i = 0; i < byteArray.length; i++)
+		{
+			//& is a bitwise AND operation 
+			int value = byteArray[i] & 0xff;
+			if (value < 16) 
+			{
+				buffer.append('0');
+			}
+			
+			buffer.append(Integer.toHexString(value));
+		}
+
+		return buffer.toString().toUpperCase();
+	}	
+	
+}
--- code/src/com/example/ap/verifier/MainActivity.java
+++ code/src/com/example/ap/verifier/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.ap.verifier;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.ap.verifier.MifareClassicParser;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	private NfcAdapter adapter;
+	private PendingIntent pendingIntent;	
+	String[][]	techLists = new String[][] { new String[] { MifareClassic.class.getName() } };	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //NFC stuff
+        adapter = NfcAdapter.getDefaultAdapter(this);
+		pendingIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);        
+        Intent intent = getIntent();
+        resolveIntent(intent);
     }
 
     @Override
@@ -17,4 +37,46 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+	public void resolveIntent(Intent intent)
+	{
+		String action = intent.getAction();
+		Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG); //Why not getParcelableArrayExtra ??
+	
+		if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action))
+		{
+			//NDef Tag
+		}
+		else if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
+		{
+		    //Get tech list (tag type) from intent
+		    String[] techList_array = tag.getTechList();
+		    List<String> techList = Arrays.asList(techList_array);
+		     
+		    if(techList.contains("android.nfc.tech.MifareClassic"))
+		    {
+		    	MifareClassicParser mcp = new MifareClassicParser();
+		    	String tagData = mcp.readMifareClassic(tag);
+		    	tagData += "\n";
+		    	
+		    	//need to actually do something with the tag data
+		    }
+		}
+	    else
+	    {
+	    	//if here, failed to recognize tag type supported by our app
+	    }  
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		adapter.disableForegroundDispatch(this);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		adapter.enableForegroundDispatch(this, pendingIntent, null, techLists);
+	}	
 }
