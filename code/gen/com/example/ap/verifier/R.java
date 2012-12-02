b97f3b833f628b7274eb5b0a908061451a5e6b06
JMK <jmk>
2012-12-02 15:23:06 -0500

    working gui

--- code/gen/com/example/ap/verifier/R.java
+++ code/gen/com/example/ap/verifier/R.java
@@ -15,18 +15,34 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int about=0x7f070007;
+        public static final int authorTitle=0x7f070002;
+        public static final int authors=0x7f070003;
+        public static final int calculate=0x7f070008;
+        public static final int menu_settings=0x7f07000a;
+        public static final int shortString=0x7f070009;
+        public static final int shortStringValue=0x7f070006;
+        public static final int ssid=0x7f070005;
+        public static final int ssidValue=0x7f070004;
+        public static final int use=0x7f070001;
+        public static final int useTitle=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_home=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_about=0x7f060000;
+        public static final int activity_home=0x7f060001;
+        public static final int activity_main=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int calculate=0x7f040005;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_about=0x7f040006;
+        public static final int title_activity_home=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
---
+++ code/src/com/example/ap/verifier/HomeActivity.java
@@ -0,0 +1,52 @@
+package com.example.ap.verifier;
+
+import com.example.ap.comparison_strings.*;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		
+		Button buttonCalc = (Button) findViewById(R.id.calculate);
+        buttonCalc.setOnClickListener(new OnClickListener()
+        {
+     	   public void onClick(View v){
+     		   ssc s = new ssc();
+     		   String comparison = s.get_comparison_string();
+     		   TextView text = (TextView) findViewById(R.id.shortStringValue);
+     		   text.setText(comparison);
+     		   }
+     	   }
+        );
+        
+        Button buttonAbout = (Button) findViewById(R.id.about);
+        buttonAbout.setOnClickListener(new OnClickListener()
+        {
+     	   public void onClick(View v){
+     		   Intent i = new Intent(HomeActivity.this, About.class); 
+     		   startActivity(i);
+     		   }
+     	   }
+        );
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_home, menu);
+		return true;
+	}
+
+}
